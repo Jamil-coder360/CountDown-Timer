6256089f35f183241cc691522e43203fe731cb64
Jamil-coder360 <jamil595nhj@gmail.com>
2026-04-07 02:37:42 +0600

    javaScript functional work done

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function updateCountdown(){
         return;
     }
 
-    // const d = Math.floor(diff / (1000 * 60 * 60 * 24));
-    // const h = Math.floor((diff / (1000 * 60 * 60)) % 24);
-    // const m = Math.floor((diff / (1000 * 60)) % 60);
-    // const s = Math.floor((diff / 1000) % 60);
+    const d = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const h = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const m = Math.floor((diff / (1000 * 60)) % 60);
+    const s = Math.floor((diff / 1000) % 60);
 
     day.innerText = d;
     hours.innerText = h;
